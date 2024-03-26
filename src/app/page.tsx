5c73e336957448c8bee6da94345496eeb92dff7c
Juliano Braz <jbraz@bionexo.com>
2024-03-26 13:08:22 -0300

    improve: load data and render component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,18 @@
+import data from "@/data.json";
+import styles from "./page.module.css";
+
+import Content from "@/models/Content";
+import Card from "@/components/card/card";
 
 export default function Home() {
   return (
-    <p>AAAAAAAAAAA</p>
+    <main className={styles.main}>
+      {data.map((x : Content, i : number)=> {
+        return <Card
+          key={x.emojiCode}
+          content={x}
+        />
+      })}
+    </main>
   );
 }
