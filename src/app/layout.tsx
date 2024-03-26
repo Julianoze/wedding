c3e45096cc12dd910af17c8fd3452240f66d29a6
Juliano Braz <jbraz@bionexo.com>
2024-03-26 13:06:59 -0300

    improve: add font and page title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Josefin+Slab:ital,wght@0,100..700;1,100..700&display=swap" rel="stylesheet" />
+        <title>E & M</title>
+      </head>
       <body>{children}</body>
     </html>
   )
