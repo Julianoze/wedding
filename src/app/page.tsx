d25f3a412b5d500fee269cb3941850ae2bde88e6
Juliano Braz <jbraz@bionexo.com>
2024-03-26 15:28:08 -0300

    improve: add grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,51 @@ import styles from "./page.module.css";
 
 import Content from "@/models/Content";
 import Card from "@/components/card/card";
+import Title from "@/components/title/title";
 
 export default function Home() {
+  const Grid = () => {
+    let element = new Array<JSX.Element>();
+
+    Array
+    .from(Array(27), (_, i) => i+1)
+    .forEach((i) => {
+      element.push(
+        <div className={styles.column}>
+          <img src={`/grid/${i}.jpeg`} />
+        </div>
+      );
+    });
+
+    return (
+      <div className={styles.grid}>
+        {element}
+      </div>
+    );
+  };
+
   return (
     <main className={styles.main}>
       {data.map((x : Content, i : number)=> {
-        return <Card
-          key={x.emojiCode}
-          content={x}
-        />
+        return (
+            <Card
+              key={i}
+              content={x}
+            />
+        )
       })}
+
+      <Title
+        emojiCode="127909"
+        text="De frente com JuZé"
+      />
+      <p>Vídeo</p>
+
+      <Title
+        emojiCode="129482"
+        text="Memórias congeladas"
+      />
+      <Grid />
     </main>
   );
 }
